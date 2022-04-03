a23e1005a262161f6a78ca3f45ee8834eed17923
Padala Deepika <padala.deepika.4@gmail.com>
2022-04-03 07:29:02 +0000

    first commit

---
+++ src/testcases/SampleTest.java
@@ -0,0 +1,15 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void test1() {
+		
+		System.out.println("Hello From Ant");
+	}
+	
+	
+
+}
